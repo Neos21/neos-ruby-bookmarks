9041f5f06795234fb5a1a4d437402594ff0cdf99
Neos21 <neos21@gmail.com>
2019-12-24 14:46:25 +0900

    v1

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# neos-ruby-bookmarks
-Ruby 製のオレオレブックマーク
+# Neo's Ruby Bookmarks
+
+Ruby 製のオレオレブックマーク。
+
+- __[Example](http://35.197.103.64/bookmarks.rb)__ (管理人しか使えません)
+
+
+## 機能
+
+- パスワード認証により、管理者だけが管理・閲覧できる、オリジナルのブックマーク。Pocket (Read It Later) 的に「読んだら削除する」使い方を想定
+- ブックマークはテキストファイルに保存する
+
+
+## 設定
+
+`index.rb` の `定数` 部分を自環境に合わせて変更する。
+
+- `PRIVATE_DIRECTORY_PATH`
+    - クレデンシャルファイルやシーケンスファイル、ブックマークファイルを格納する「プライベートディレクトリ」のパス。Ruby の配置先から見た相対パスで記載できる
+    - ex. Apache サーバの `/var/www/html/` に `index.rb` を配置した場合、`'../private'` と指定すれば、`/var/www/private/` ディレクトリ配下を参照するようになる
+- `CREDENTIAL_FILE_PATH`
+    - アクセスパスワードを書いた「クレデンシャルファイル」の名前を記す
+    - `PRIVATE_DIRECTORY_PATH` と結合して参照するので、デフォルトの記述のままでいけば `/var/www/private/credential.txt` を参照することになる
+- `SEQUENCE_FILE_PATH`
+    - ブックマークのシーケンス値を管理する「シーケンスファイル」の名前を記す
+    - `PRIVATE_DIRECTORY_PATH` と結合するので、デフォルトの記述のままでいけば `/var/www/private/bookmarks-sequence.txt` といったファイルが生成される
+    - ファイルが存在しなかった場合は `0` を記入したファイルを生成する
+- `BOOKMARKS_FILE_PATH`
+    - ブックマーク一覧を保存する「ブックマークファイル」の名前を記す
+    - `PRIVATE_DIRECTORY_PATH` と結合するので、デフォルトの記述のままでいけば `/var/www/private/bookmarks.txt` といったファイルが生成される
+    - ファイルが存在しなかった場合は空ファイルを生成する
+- `PAGE_TITLE`
+    - `title` 要素、および `h1` 要素で示されるページタイトル
+
+
+## 導入方法
+
+1. Apache サーバで Ruby を CGI として利用できるように設定する
+2. Apache サーバの `/var/www/html/` 配下などに `index.rb` を配置する
+3. 変数 `PRIVATE_DIRECTORY_PATH` + `CREDENTIAL_FILE_PATH` のパスに、アクセスパスワードを記した1行のテキストファイル (クレデンシャルファイル) を作る
+4. `index.rb`、プライベートディレクトリ、クレデンシャルファイル、シーケンスファイル、ブックマークファイルのパーミッションを適宜設定する
+5. `index.rb` にアクセスする
+
+
+## 管理者閲覧の方法
+
+URL に `credential` パラメータを指定してアクセスすると、ブックマーク一覧が表示できる。
+
+- ex. `http://example.com/index.rb?credential=MY_CREDENTIAL`
+
+`credential` パラメータで指定したパスワードの整合性は、「クレデンシャルファイル」と突合して確認する。
+
+管理者用画面では、新規ブックマークの追加と、ブックマーク一覧の表示、ブックマークの削除が行える。
+
+
+## ブックマークレットでブックマークを追加する
+
+次のようなブックマークレットを用意することで、閲覧中のページを新規ブックマークとして追加できるようになる。
+
+```javascript
+javascript:(()=>{window.open('http://example.com/index.rb?credential=MY_CREDENTIAL&mode=add&title='+encodeURIComponent(document.title)+'&url='+encodeURIComponent(document.URL));})();
+```
+
+
+## Author
+
+[Neo](http://neo.s21.xrea.com/) ([@Neos21](https://twitter.com/Neos21))
+
+
+## Links
+
+- [Neo's World](http://neo.s21.xrea.com/)
+- [Corredor](http://neos21.hatenablog.com/)
+- [Murga](http://neos21.hatenablog.jp/)
+- [El Mylar](http://neos21.hateblo.jp/)
+- [Neo's GitHub Pages](https://neos21.github.io/)
+- [GitHub - Neos21](https://github.com/Neos21/)
---
+++ index.rb
@@ -0,0 +1,416 @@
+#!/usr/bin/ruby
+
+# ======================================================================
+# Neo's Ruby Bookmarks
+#
+# Neo (@Neos21) http://neo.s21.xrea.com/
+# ======================================================================
+
+
+# Bookmarklet
+# ======================================================================
+
+# javascript:(()=>{window.open('http://【HostName】/index.rb?credential=【Credential】&mode=add&title='+encodeURIComponent(document.title)+'&url='+encodeURIComponent(document.URL));})();
+
+
+# Requires
+# ======================================================================
+
+require 'cgi'
+require 'fileutils'
+
+
+# 定数
+# ======================================================================
+
+# Private ディレクトリのパス (末尾スラッシュなし) : この配下に「クレデンシャルファイル」「シーケンスファイル」「ブックマークファイル」を配置する
+PRIVATE_DIRECTORY_PATH = '../private'
+# アクセスパスワードを管理するクレデンシャルファイルのパス
+CREDENTIAL_FILE_PATH = "#{PRIVATE_DIRECTORY_PATH}/credential.txt"
+# ブックマークのシーケンス値を保存するファイルのパス
+SEQUENCE_FILE_PATH = "#{PRIVATE_DIRECTORY_PATH}/bookmarks-sequence.txt"
+# ブックマーク一覧を保存するファイルのパス
+BOOKMARKS_FILE_PATH = "#{PRIVATE_DIRECTORY_PATH}/bookmarks.txt"
+# タイトル
+PAGE_TITLE = "Neo's Ruby Bookmarks";
+
+
+# グローバル変数
+# ======================================================================
+
+# CGI オブジェクト
+$cgi = CGI.new
+
+
+# 事前処理
+# ======================================================================
+
+def main()
+  # クレデンシャルが不正な場合
+  unless is_valid_credential
+    print_error('<p><strong>Access Denied</strong></p>')
+    return
+  end
+  
+  # シーケンスファイルとブックマークファイルの存在チェック・なければ空ファイルを作成する
+  unless File.exist?(SEQUENCE_FILE_PATH)
+    File.open(SEQUENCE_FILE_PATH, 'w:UTF-8') { |sequence_file|
+      sequence_file.puts 0
+    }
+  end
+  unless File.exist?(BOOKMARKS_FILE_PATH)
+    FileUtils.touch(BOOKMARKS_FILE_PATH)
+  end
+  
+  mode = $cgi['mode']
+  if mode == 'add'
+    add
+  elsif mode == 'remove'
+    remove
+  else
+    show
+  end
+end
+
+# パラメータのクレデンシャルが正しいかチェックする
+def is_valid_credential()
+  credential_param = $cgi['credential']
+  
+  credential = ''
+  File.open(CREDENTIAL_FILE_PATH, 'r:UTF-8') { |credential_file|
+    credential = credential_file.read.chomp
+  }
+  
+  return credential_param == credential
+end
+
+
+# モード別の関数
+# ======================================================================
+
+# 一覧を表示する
+def show()
+  print_header
+  
+  # 登録・削除フォーム
+  print(<<"EOL")
+<form action="#{File.basename(__FILE__)}" method="GET" id="add-form">
+  <input type="hidden" name="credential" value="#{$cgi['credential']}">
+  <input type="hidden" name="mode"       value="add">
+  <p>
+    <input type="text" name="title" id="add-title" value="" placeholder="Title">
+    <input type="text" name="url"   id="add-url"   value="" placeholder="URL">
+    <input type="button" id="add-btn" value="Add">
+  </p>
+</form>
+<form action="#{File.basename(__FILE__)}" method="GET" id="remove-form">
+  <input type="hidden" name="credential" value="#{$cgi['credential']}">
+  <input type="hidden" name="mode"       value="remove">
+  <input type="hidden" name="targets"    value="" id="remove-targets">
+  <p>
+    <input type="button" id="remove-btn"    value="Remove">
+    <input type="button" id="check-all-btn" value="All">
+    <span id="feedback"></span>
+  </p>
+</form>
+EOL
+  
+  # 一覧表示
+  is_empty = true
+  File.open(BOOKMARKS_FILE_PATH, 'r:UTF-8') { |bookmarks_file|
+    bookmarks_file.each_line { |line|
+      if line.chomp.empty?
+        next
+      end
+      columns  = line.chomp.split("\t")
+      sequence = columns[0]
+      title    = CGI.escapeHTML(columns[1])
+      url      = CGI.escapeHTML(columns[2])
+      print(<<"EOL")
+<label class="list-item">
+  <input type="checkbox" name="sequences" value="#{sequence}">
+  <span>#{sequence} : </span>
+  <a href="#{url}" target="_blank">[ #{title} ]</a>
+</label>
+EOL
+      is_empty = false
+    }
+  }
+  
+  if is_empty
+    puts('<p>No Bookmarks</p>')
+  end
+  
+  print(<<"EOL")
+<script>
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('add-btn').addEventListener('click', () => {
+    document.getElementById('feedback').innerHTML = '';
+    if(!document.getElementById('add-title').value.trim() || !document.getElementById('add-url').value.trim()) {
+      document.getElementById('feedback').innerHTML = 'Title or URL is empty';
+      return;
+    }
+    document.getElementById('add-form').submit();
+  });
+  
+  document.getElementById('remove-btn').addEventListener('click', () => {
+    document.getElementById('feedback').innerHTML = '';
+    
+    // チェックされているアイテムのシーケンス値をカンマ区切りで取得する
+    let sequences = '';
+    document.querySelectorAll('input[name="sequences"]').forEach((checkbox) => {
+      if(checkbox.checked) {
+        sequences += `${sequences ? ',' : ''}${checkbox.value}`;
+      }
+    });
+    
+    // 一つもチェックされていなければ何もしない
+    if(!sequences) {
+      document.getElementById('feedback').innerHTML = 'Please check at least 1';
+      return;
+    }
+    
+    // 隠しフォームに削除対象のシーケンス値を設定し Submit する
+    document.getElementById('remove-targets').value = sequences;
+    document.getElementById('remove-form').submit();
+  });
+  
+  document.getElementById('check-all-btn').addEventListener('click', () => {
+    document.querySelectorAll('input[name="sequences"]').forEach((checkbox) => {
+      checkbox.checked = true;
+    });
+  });
+});
+
+</script>
+EOL
+  print_footer
+end
+
+# 追加する
+def add()
+  # パラメータより追加対象のページ情報を取得する
+  raw_title = $cgi['title']
+  raw_url   = $cgi['url']
+  if raw_title.empty? || raw_url.empty?
+    print_error('Add : Title or URL is empty')
+    return
+  end
+  
+  # 現在のシーケンス値を取得し次のシーケンス値を作る
+  next_sequence = 0
+  File.open(SEQUENCE_FILE_PATH, 'r:UTF-8') { |sequence_file|
+    current_sequence = sequence_file.read.chomp.to_i
+    next_sequence = current_sequence + 1
+  }
+  
+  # ブックマークに追記する
+  File.open(BOOKMARKS_FILE_PATH, 'a:UTF-8') { |bookmarks_file|
+    # 書き込み用にデコードする・タブ文字は適宜変換する
+    title = CGI.unescape(raw_title).gsub("\t", ' ')
+    url   = CGI.unescape(raw_url).gsub("\t", '%09')
+    bookmarks_file.puts "#{next_sequence}\t#{title}\t#{url}"
+  }
+  
+  # シーケンス値を更新する
+  File.open(SEQUENCE_FILE_PATH, 'w:UTF-8') { |sequence_file|
+    sequence_file.puts next_sequence
+  }
+  
+  # 追加後は一覧にリダイレクトする
+  puts($cgi.header({
+    'status' => 'REDIRECT',
+    'location' => create_redirect_url
+  }))
+end
+
+# 削除する
+def remove()
+  # 削除対象のシーケンス値を取得する
+  raw_targets = $cgi['targets']
+  if raw_targets.empty?
+    print_error('Remove : Targets not exist')
+    return
+  end
+  
+  # 削除対象のシーケンス値をカンマで区切る
+  targets = raw_targets.split(',')
+  
+  # ブックマークファイルを開き、削除対象以外の行を取得していく
+  new_text = ''
+  File.open(BOOKMARKS_FILE_PATH, 'r:UTF-8') { |bookmarks_file|
+    bookmarks_file.each_line { |line|
+      columns  = line.chomp.split("\t")
+      sequence = columns[0]
+      unless targets.include?(sequence)
+        new_text << line
+      end
+    }
+  }
+  
+  # ファイルを更新する
+  File.open(BOOKMARKS_FILE_PATH, 'w:UTF-8') { |bookmarks_file|
+    bookmarks_file.puts(new_text)
+  }
+  
+  # 削除後は一覧にリダイレクトする
+  puts($cgi.header({
+    'status' => 'REDIRECT',
+    'location' => create_redirect_url
+  }))
+end
+
+
+# 共通関数
+# ======================================================================
+
+# HTML のヘッダ部分を出力する
+def print_header()
+  print(<<"EOL")
+content-type: text/html
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>#{PAGE_TITLE}</title>
+    <style>
+
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, YuGothic, "Yu Gothic", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "Hiragino Kaku Gothic Pro", Meiryo, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  text-decoration-skip-ink: none;
+  -webkit-text-size-adjust: 100%;
+  -webkit-text-decoration-skip: objects;
+  word-break: break-all;
+  line-height: 1.5;
+  background: #000;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
+  cursor: default;
+}
+
+html,
+a {
+  color: #0d0;
+}
+
+h1 {
+  font-size: 1rem;
+}
+
+h1 a {
+  text-decoration: none;
+}
+
+h1 a:hover {
+  text-decoration: underline;
+}
+
+input {
+  margin: 0;
+  border: 1px solid #0c0;
+  border-radius: 0;
+  padding: .25rem .5rem;
+  color: inherit;
+  font-size: 1rem;
+  font-family: inherit;
+  background: transparent;
+  vertical-align: top;
+  outline: none;
+}
+
+input::placeholder {
+  color: #090;
+}
+
+#add-form > p {
+  display: grid;
+  grid-template: "add-title add-submit" auto
+                 "add-url   add-submit" auto
+                 / 1fr auto;
+}
+
+#add-form > p > input:nth-child(1) {
+  grid-area: add-title;
+}
+
+#add-form > p > input:nth-child(2) {
+  grid-area: add-url;
+  border-top: 0;
+}
+
+#add-form > p > input:nth-child(3) {
+  grid-area: add-submit;
+  border-left: 0;
+}
+
+#remove-form > p {
+  margin-top: 1.5rem;
+}
+
+#remove-form > p > #feedback {
+  margin-left: 1rem;
+  font-weight: bold;
+  vertical-align: bottom;
+}
+
+.list-item {
+  display: grid;
+  grid-template: auto / auto auto 1fr;
+  align-items: start;
+}
+
+.list-item > input {
+  position: relative;
+  top: .25rem;
+}
+
+.list-item > span {
+  margin-left: .5rem;
+  margin-right: 1rem;
+}
+
+    </style>
+  </head>
+  <body>
+EOL
+end
+
+# HTML のフッタ部分を出力する
+def print_footer()
+  print(<<'EOL')
+  </body>
+</html>
+EOL
+end
+
+# エラーメッセージを表示する
+def print_error(message)
+  print_header
+  print(<<"EOL")
+<h1>#{PAGE_TITLE}</h1>
+<p><strong>#{message}</strong></p>
+EOL
+  print_footer
+end
+
+# リダイレクト用 URL を生成する
+def create_redirect_url()
+  return "http://#{ENV['SERVER_NAME']}/#{File.basename(__FILE__)}?credential=#{$cgi['credential']}"
+end
+
+
+# 実行
+# ======================================================================
+
+main
